501a74227ff207b0955fce0965aba88895b8f221
nicholas wooldridge <wooldrid@hoare7.cs.umsl.edu>
2018-12-04 01:08:55 -0600

    Building framework still. Currently having issues with getting shmctl() to work. Good stuff.

--- oss.c
+++ oss.c
@@ -4,71 +4,209 @@
 #include <sys/msg.h>
 #include <time.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <limits.h>
+
+//Queue structure
+//Source: https://www.geeksforgeeks.org/queue-set-1introduction-and-array-implementation/
+struct Queue 
+{ 
+    int front, rear, size; 
+    unsigned capacity; 
+    int* array; 
+}; 
+
+//create queue function
+struct Queue* createQueue(unsigned capacity) 
+{ 
+    struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue)); 
+    queue->capacity = capacity; 
+    queue->front = queue->size = 0;  
+    queue->rear = capacity - 1;  // This is important, see the enqueue 
+    queue->array = (int*) malloc(queue->capacity * sizeof(int)); 
+    return queue; 
+} 
+
+//return bool value if full or not
+int isFull(struct Queue* queue) {
+	return (queue->size == queue->capacity); 
+} 
+
+//return bool value if empty or not
+int isEmpty(struct Queue* queue) {
+return (queue->size == 0);
+}
+
+//enqueue item at rear of queue
+void enqueue(struct Queue* queue, int item) 
+{ 
+    if (isFull(queue)) 
+        return; 
+    queue->rear = (queue->rear + 1)%queue->capacity; 
+    queue->array[queue->rear] = item; 
+    queue->size = queue->size + 1; 
+    printf("%d enqueued to queue\n", item); 
+} 
+
+//dequeue item from front of queue
+int dequeue(struct Queue* queue) 
+{ 
+    if (isEmpty(queue)) 
+        return INT_MIN; 
+    int item = queue->array[queue->front]; 
+    queue->front = (queue->front + 1)%queue->capacity; 
+    queue->size = queue->size - 1; 
+    return item; 
+}
+ 
+//return front of queue
+int front(struct Queue* queue) 
+{ 
+    if (isEmpty(queue)) 
+        return INT_MIN; 
+    return queue->array[queue->front]; 
+}
+ 
+//return rear of queue
+int rear(struct Queue* queue) 
+{ 
+    if (isEmpty(queue)) 
+        return INT_MIN; 
+    return queue->array[queue->rear]; 
+}  
 
 //struct for keeping track of child processes
 struct process {
+	//process id
 	pid_t processID;
 	//array to represent virtual address space
 	int pageTable[32];
 };
 typedef struct process process;
 
+struct frame {
+	//reference bit for second-chance FIFO page replacement algorithm
+	int referenceBit;
+	//dirty bit
+	int dirtyBit;
+	
+};
+typedef struct frame frame;
+
 //message queue structure
 struct mesgBuffer {
         long mesgType;
-        char mesg[20];
+	//Process id
+        pid_t pid;
+	//Number from 0 - 32
+	int pageNumber;
+	//0 for read, 1 for write
+	int readOrWrite;
 } message;
 
-key_t msgKey;
-int msgid;
+key_t msgKey, shmKey;
+int msgid, shmid;
+process * PCB;
+
+//size of PCB
+const int MAX_PROCESSES = 18;
 
 int main(int argc, char ** argv) {
 	//loop counters
 	int i, j;
-
-	//Create key for msgqueue
+	
+	//Create key for msgqueue	
 	msgKey = ftok("msgqfile", 51);
+	
+	//Create key for shared memory
+	shmKey = ftok("shmfile", 12);
+
+	//error checking for ftok
+	if (msgKey == -1 || shmKey == -1)
+		perror("ftok: ");
 	
 	//create message queue
 	msgid = msgget(msgKey, 0666|IPC_CREAT);
 	
+	//create shared memory segment for process control blocks
+	shmid = shmget(shmKey, MAX_PROCESSES*sizeof(process), 0666|IPC_CREAT);
+	
+	//error checking for msgget and shmget	
 	if (msgid == -1)
-		perror("msgget oss: \n");
+		perror("msgget oss: ");
 	
-	//array to represent physical address space
-	int frames[256];
+	if (shmid == -1)
+		perror("shmget oss: ");
 	
-	//0 in the array means the "frame" is unallocated
-	for (i = 0; i != 256; i += 1) {
-		frames[i] = 0;
+	//attach shared memory segment to process control block pointer
+	PCB = (process * )shmat(shmid, (void *)0, 0);
+	
+	//initialize PCB's
+	for (i = 0; i != MAX_PROCESSES; i += 1) {
+		
+		//set all process id's to 0	
+		PCB[i].processID = 0;
+
+		//set page tables to contain all -1
+		for (j = 0; j != 32; j += 1)
+			PCB[i].pageTable[j] = -1;
+
 	}
 	
-	message.mesgType = 100;
+	//array to represent physical address space
+	frame frames[256];
 	
-	strncpy(message.mesg, "hello there\0", sizeof(message.mesg));
+	//initialize frame table
+	for (i = 0; i != 256; i += 1) {
+		frames[i].referenceBit = 0;
+		frames[i].dirtyBit = 0;
+	}
 	
+	message.mesgType = 1;
+	
+		
+	/*
 	if (msgsnd(msgid, &message, sizeof(message), 0))
 		perror("msgsnd oss");
+	*/
 	
-	printf("%s\n", message.mesg);
 		
-	pid_t childpid = fork();
+	//fork off
 	
+	pid_t childpid = fork();
+		
 	if (childpid == -1)
 		perror("fork: ");
 	else if (childpid == 0) {
 		//in child
+		
+		//set process id in PCB
+		for (i = 0; i != MAX_PROCESSES; i += 1){
+			if (PCB[i].processID == 0)
+				PCB[i].processID = getpid();
+		}
+
+		
 		execlp("./user", "user", NULL);
 		perror("execlp: ");
 	}
-	
 
-	wait(0);
-	
+	pid_t cpid = PCB[0].processID;
+	//receive message from user	
+	if (msgrcv(msgid, &message, sizeof(message), cpid, 1) < 0)
+		perror("msgrcv: ");
+	printf("\nhello123!");	
+
+		
+		
 	
 	//Destroy message queue
 	if (msgctl(msgid, IPC_RMID, NULL) == -1)
                 perror("msgctl oss: ");
-		
+	
+	if (shmctl(shmid, IPC_RMID, NULL) == -1)
+		perror("shmctl oss: ");	
+	if (shmdt(PCB) == -1)
+		perror("shmdt oss: ");		
 	return 0;
 }
--- user.c
+++ user.c
@@ -8,29 +8,50 @@
 
 struct mesgBuffer {
         long mesgType;
-        char mesg[20];
+        //process id
+        pid_t pid;
+	//page number
+	int pageNumber;
+	//0 for read, 1 for write
+	int readOrWrite;
+
 } message;
  
 int main(int argc, char ** argv) {
-	
+	printf("hello from user!\n");
 	//loop counters
 	int i, j;
 	
+	//setting up  message queues
 	key_t msgKey = ftok("msgqfile", 51);	
-	
+
+	if (msgKey == -1)
+		perror("ftok user: ");
+
 	int msgid = msgget(msgKey, 0);
+ 
 
 	if (msgid == -1)
 		perror("msgget user\n");
 
-	if (msgrcv(msgid, &message, sizeof(message), 100, 0) < 0)
+	/*
+	if (msgrcv(msgid, &message, sizeof(message), getpid(), 0) < 0)
 		perror("msgrcv user: \n");
+	*/
 	
-	printf("%s\n", message.mesg);
-			
+	message.mesgType = getpid();
+	
+	if (msgsnd(msgid, &message, sizeof(message), 0) < 0)
+		perror("msgsnd user:");
+	
+	
+	
 	//seeding random
 	srand(time(NULL));
-
+	
+	int memoryReference = rand() % 32 + 1;
+	
+		
 
 	return 0;
 }
